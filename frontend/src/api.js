cdee90d788b7b43602455014b8ae1af48b22c831
Sankalpa_01 <sankalpaanda@gmail.com>
2025-08-19 14:14:22 +0530

    Backend deployed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({
+  baseURL: "https://leave-management-a71y.onrender.com/api",
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("user")) {
